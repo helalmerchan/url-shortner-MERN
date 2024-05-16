a594aab635ffa0a180f546c19dde9bfe3c379e46
helalmerchan <helalmerchan@gmail.com>
2024-05-16 08:15:27 +0600

    code 30%

---
+++ server-app/src/controllers/shorlUrl.ts
@@ -0,0 +1,42 @@
+import express from "express";
+import { urlModel } from "../model/shortUrl";
+
+export const createUrl = async (req: express.Request, res: express.Response) => {
+    try {
+        console.log("The full URL is ", req.body.fullUrl);
+        const { fullUrl } = req.body;
+        const urlFound = await urlModel.find({ fullUrl });
+        if (urlFound.length > 0) {
+            res.status(409);
+            res.send(urlFound);
+        } else {
+            const shortUrl = await urlModel.create({ fullUrl });
+            res.status(201).send(shortUrl);
+        }
+    } catch (error) {
+        res.status(500).send({ message: `Something went wrong ${error}` });
+    }
+};
+
+export const getAllUrl = async (req: express.Request, res: express.Response) => {
+    try {
+        const shortUrls = await urlModel.find();
+        if (shortUrls.length < 0) {
+            res.status(404).send({ message: "Short url Not found" });
+        } else {
+            res.status(200).send(shortUrls);
+        }
+    } catch (error) {
+        res.status(500).send({ message: `Something went wrong ${error}` });
+    }
+};
+
+export const getUrl = async (req: express.Request, res: express.Response) => {
+
+};
+
+
+export const deleteUrl = async (
+    req: express.Request, res: express.Response
+
+) => { }
